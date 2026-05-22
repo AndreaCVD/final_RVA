01ce37c8aa4b85e4f4aeea4f9d7d762f247a7fd3
lasorr <laia.sorribes@estudiantat.upc.edu>
2026-05-22 18:28:01 +0200

    Salsa - scene Particles - yes

--- Assets/G4_FinalProject/Scripts/ParticlesOnColl.cs
+++ Assets/G4_FinalProject/Scripts/ParticlesOnColl.cs
@@ -2,15 +2,55 @@ using UnityEngine;
 
 public class ParticlesOnColl : MonoBehaviour
 {
+    public GameObject MeshPizza;
     public Material TomatoSauce;
-    //public Material WhiteSauce;
+    public Material WhiteSauce;
+    public Material Pimineta;
+
+    private bool Tomatoe = false;
+    private bool White = false;
+    private bool Pepper = false;
 
     private void OnParticleCollision(GameObject other)
     {
         Debug.Log($"Hay colision con: {other?.name}");
         if (other.CompareTag("SalsaTomatoe"))
         {
-            gameObject.GetComponent<MeshRenderer>().material = TomatoSauce;
+            if(Tomatoe == true)
+            {
+                return;
+            }
+            else
+            {
+                MeshPizza.GetComponent<MeshRenderer>().material = TomatoSauce;
+                Tomatoe = true;
+            }
+                
+        }
+        else if (other.CompareTag("WitheSauce"))
+        {
+            if (White == true)
+            {
+                return;
+            }
+            else
+            {
+                MeshPizza.GetComponent<MeshRenderer>().material = WhiteSauce;
+                White = true;
+            }
+                
+        }
+        else if (other.CompareTag("Pepper"))
+        {
+            if (Pepper == true)
+            {
+                return;
+            }
+            else
+            {
+                MeshPizza.GetComponent<MeshRenderer>().material = Pimineta;
+                Pepper = true;
+            }
         }
     }
 }
